6a1098671fb76baa32f26ec99b80c35a01f86c3a
Shadi Aslan <70282602+shadilios@users.noreply.github.com>
2022-03-26 16:57:58 +0300

    Copy starter code

---
+++ Tic-Tac-Toe/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+	class Board
+	{
+		/// <summary>
+		/// Tic Tac Toe Gameboard states
+		/// </summary>
+		public string[,] GameBoard = new string[,]
+		{
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+
+		public void DisplayBoard()
+		{
+
+			//TODO: Output the board to the console
+
+		}
+	}
+}
---
+++ Tic-Tac-Toe/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+	class Position
+	{
+		public int Row { get; set; }
+		public int Column { get; set; }
+
+		/// <summary>
+		/// Position on the gameboard being initialized 
+		/// </summary>
+		/// <param name="row">row number</param>
+		/// <param name="column">column number</param>
+		public Position(int row, int column)
+		{
+			Row = row;
+			Column = column;
+		}
+	}
+}
